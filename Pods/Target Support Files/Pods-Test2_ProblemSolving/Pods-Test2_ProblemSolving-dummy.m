49a813f11e1f08c4d0d0c09f58c4b915d1e45b75
Thanaphat Suwannikornkul <onsensei@gmail.com>
2019-02-16 15:09:08 +0700

    init

---
+++ Pods/Target Support Files/Pods-Test2_ProblemSolving/Pods-Test2_ProblemSolving-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Test2_ProblemSolving : NSObject
+@end
+@implementation PodsDummy_Pods_Test2_ProblemSolving
+@end
---
+++ Test2_ProblemSolving/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  Test2_ProblemSolving
+//
+//  Created by Softcake on 25/4/2561 BE.
+//  Copyright © 2561 AppMan. All rights reserved.
+//
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var boxView: UIView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func didPress(cameraButton: UIButton) {
+        let vc = UIImagePickerController()
+        vc.delegate = self
+        vc.sourceType = .camera
+        vc.cameraCaptureMode = .photo
+        present(vc, animated: true, completion: nil)
+    }
+}
+
